52f670d217c1d2155fb482aaf3cc210402af3c44
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-05-28 07:22:39 +0200

    feat: Add API endpoint to receive POST requests and compare workflow with catalog
    The code changes in this commit add a new API endpoint in the `views.py` file. This endpoint is responsible for receiving POST requests and comparing the workflow with the catalog. The `postData` function now receives the request body, extracts the workflow and catalog data, and passes them to the `compare_workflow_and_catalog` function. The result of the comparison is included in the response.

---
+++ app/api/lib/workflow_catalog_evaluator.py
@@ -0,0 +1,5 @@
+def compare_workflow_and_catalog(workflow, catalog):
+    """
+    Compare a workflow and a catalog
+    """
+    return 1
--- app/api/views.py
+++ app/api/views.py
@@ -2,28 +2,27 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
 import json
-
-@api_view(['GET'])
-def getData(request):
-    data = {
-        'name': 'John Doe',
-        'age': 25,
-        'is_student': True
-    }
-    return Response(data)
+from .lib.workflow_catalog_evaluator import compare_workflow_and_catalog
 
 # Print data posted to the server
 @api_view(['POST'])
 def postData(request):
-    # Pretty print the request body
-        request_body = json.loads(request.body)
-        pretty_request_body = json.dumps(request_body, indent=4)
-        print(pretty_request_body)
+    # Recieve data from POST request
+    request_body = json.loads(request.body)
+    workflow = request_body[0]
+    print(f'Workflow: {workflow}')
+    
+    catalog = request_body[1]
+    print(f"Catalog: {catalog}")
+    
+    result = compare_workflow_and_catalog(workflow, catalog)
+    # pretty_request_body = json.dumps(request_body, indent=4)
         
-        # Define response
-        response = JsonResponse(
-            {
-                'message': 'Workflow and catalog successfully recieved'
-            }, 
-            status=200)
-        return response
+    # Define response
+    response = JsonResponse(
+        {
+            'message': 'Workflow and catalog successfully recieved',
+            'result': result
+        }, 
+        status=200)
+    return response
