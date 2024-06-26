a1b152804c31ef0d6d7548457d012a0282dac02c
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-06-26 11:06:39 +0200

    Solved error where the css was not configured correctly in the statics

--- app/api/lib/workflow_catalog_evaluator.py
+++ app/api/lib/workflow_catalog_evaluator.py
@@ -5,4 +5,4 @@
     :return: A dictionary of the services selected for each node in the workflow
 """
 def compare_workflow_and_catalog(workflow, catalog):
-    return None
+    return None # TODO connect to the api's in solver.py -> process_workflow_and_catalog
