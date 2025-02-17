d677784815b6d542338b566edff56d7af2ca498b
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-02-17 10:47:43 +0100

    Successfully solved importing bugs (1)

--- app/api/solving_strategy/gptsolver/config.py
+++ app/api/solving_strategy/gptsolver/config.py
@@ -1,15 +1,18 @@
+import os
+
+# Get absolute path to the current directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 model_config: dict = {
-    # Takes the model choice from the command line argument
     "SELECTED_MODEL": 3,
-    "MODEL_CHOICES" : {
-        1: "gpt-3.5-turbo", # Default model
-        2: "gpt-4o",
+    "MODEL_CHOICES": {
+        1: "gpt-3.5-turbo",
+        2: "gpt-4o", 
         3: "o1-mini"
     },
-    # Number of services to be generated for each abstract service (default: 5)
     "N_OF_SERVICES": 5,
     "TEMPERATURE": 0.0,
     "DEVELOPER_TEXT": "I want you to be my AWS Architecture expert. I have a workflow that I want to deploy on AWS.",
-    "PERFORMANCE_PATH": './data/performance.json',
-    "USE_TAGS": True # If True, the tags of the abstract service will be used to find the best concrete service from the catalog
+    "PERFORMANCE_PATH": os.path.join(BASE_DIR, 'data', 'performance.json'),
+    "USE_TAGS": True
 }
